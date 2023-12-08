6130aa9a74170957587c28f893d2a3410b3486b8
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-08 17:24:22 -0500

    wrapping page context in BS container

--- btc-trakkr/src/pages/NotFound.jsx
+++ btc-trakkr/src/pages/NotFound.jsx
@@ -1,21 +1,26 @@
 import { Link } from "react-router-dom";
 import { AiTwotoneHome } from "react-icons/ai";
 import { IconContext } from "react-icons";
+import Container from "react-bootstrap/Container";
 
 const NotFound = () => {
   return (
-    <div className="container-fluid">
-        <div className="text-center">
-            <h1 className="fs-1 fw-bold mb-3">Oops!</h1>
-            <p className="fs-3 mb-2">404 - Page Not Found</p>
+    <Container fluid>
+        <Container className="text-center p-5">
+            <h1 className="fs-1 fw-bold mb-3">
+              Oops!
+            </h1>
+            <p className="fs-3 mb-2">
+              404 - Page Not Found
+            </p>
             <Link to="/" className="btn btn-lg btn-secondary d-inline-flex align-items-center">
                 <IconContext.Provider value={{ color: "orange", size: 20 }}>
-                    <AiTwotoneHome />
+                  <AiTwotoneHome />
                 </IconContext.Provider>
                 Back to Home
             </Link>
-        </div>
-    </div>
+        </Container>
+    </Container>
   );
 };
 
