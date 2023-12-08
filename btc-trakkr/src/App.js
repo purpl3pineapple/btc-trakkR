5316a9684984e97ef69aecd0dc9d76511e413bdd
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-08 12:53:37 -0500

    adding routes to pages

--- btc-trakkr/src/App.js
+++ btc-trakkr/src/App.js
@@ -1,7 +1,10 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import TrakkrNavbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Container from "react-bootstrap/Container";
+import MempoolPage from "./pages/MempoolPage";
+import NotFound from "./pages/NotFound";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -9,7 +12,13 @@ function App() {
       <Router>
         <Container fluid className="d-flex flex-column align-items-between p-0 vh-100">
           <TrakkrNavbar />
-          <Container fluid id="main" className="pb-5"> Content </Container>
+          <Container fluid id="main" className="d-flex justify-content-center h-100 pb-5">
+            <Routes>
+              <Route path="/" element={<HomePage />}/>
+              <Route path="/mempool" element={<MempoolPage />}/>
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </Container>
           <Footer />
         </Container>
       </Router>
