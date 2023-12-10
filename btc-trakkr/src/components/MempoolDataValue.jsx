a7a197f20d84799d8b9160d48aad9e82aa6a6c48
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-10 16:09:09 -0500

    adding small-text font class from scss

--- btc-trakkr/src/components/MempoolDataValue.jsx
+++ btc-trakkr/src/components/MempoolDataValue.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import Card from 'react-bootstrap/Card';
 
 const MempoolDataValue = ({ title, value }) => {
   return (
-    <Card className='mempool-card h-auto mx-3 flex-shrink-1 rounded-circle'>
+    <Card className='mempool-card h-auto m-3 p-2 flex-shrink-1 rounded-circle'>
       <Card.Body>
-        <Card.Title className="fw-bold fs-6">
+        <Card.Title className="fw-bold fs-7">
           {title}
         </Card.Title>
-        <Card.Text className="fs-6">
+        <Card.Text className="fs-7">
           {value}
         </Card.Text>
       </Card.Body>
