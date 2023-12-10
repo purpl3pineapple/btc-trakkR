841ef9908ddade7fcea547b99cc9571c91d35560
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-10 16:11:09 -0500

    adding structure & params to data section component

--- btc-trakkr/src/components/layout/MempoolDataSection.jsx
+++ btc-trakkr/src/components/layout/MempoolDataSection.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import Container from 'react-bootstrap/Container';
 
-const MempoolDataSection = () => {
+const MempoolDataSection = ({ title, content }) => {
   return (
-    <div>MempoolDataSection</div>
+    <Container className="mempool-data-container d-flex flex-column justify-content-center text-center p-3 my-5 bg-body-tertiary rounded">
+      <h1 className="mb-3 container-fluid mempool-data-header">
+        <span className="bg-info fs-2 fw-bold p-3 my-4 rounded d-inline-block text-decoration-underline shadow text-reset">
+          {title}
+        </span>
+      </h1>
+      {content}
+    </Container>
   );
 };
 
