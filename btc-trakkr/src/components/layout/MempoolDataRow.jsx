1225b5ad2ab99856879a13217761ea082c1a0a90
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-10 16:09:34 -0500

    adding mempool data row layout component

---
+++ btc-trakkr/src/components/layout/MempoolDataRow.jsx
@@ -0,0 +1,16 @@
+import Row from "react-bootstrap/Row";
+import MempoolDataValue from "../MempoolDataValue";
+
+const MempoolDataRow = ({ data }) => {
+  return (
+    <Row xs={1} md={4} className="g-4 flex-nowrap p-4">
+        {data.map((item, idx) => <MempoolDataValue
+            key={idx}
+            title={item.title} 
+            value={item.value} 
+        />)}
+    </Row>
+  );
+};
+
+export default MempoolDataRow;
