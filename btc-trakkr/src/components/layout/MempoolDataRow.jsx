850366f2e38b5f25bc9ce372c67664f541038e90
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-12 11:00:34 -0500

    mempool data row setup

--- btc-trakkr/src/components/layout/MempoolDataRow.jsx
+++ btc-trakkr/src/components/layout/MempoolDataRow.jsx
@@ -2,15 +2,16 @@ import Row from "react-bootstrap/Row";
 import MempoolDataValue from "../MempoolDataValue";
 
 const MempoolDataRow = ({ data }) => {
-  return (
-    <Row xs={1} md={4} className="g-4 flex-nowrap p-4">
-        {data.map((item, idx) => <MempoolDataValue
-            key={idx}
-            title={item.title} 
-            value={item.value} 
-        />)}
-    </Row>
-  );
+
+    return (
+        <Row xs={1} md={4} className="g-4 flex-nowrap p-4 justify-content-around">
+            {data.map((item, idx) => <MempoolDataValue
+                key={idx}
+                title={item.title} 
+                value={item.value} 
+            />)}
+        </Row>
+    );
 };
 
 export default MempoolDataRow;
