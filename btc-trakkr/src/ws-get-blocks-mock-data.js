91eac761271f42635d063312858f2efe00d4ebe9
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-12 10:57:13 -0500

    mock data from websocket requests

---
+++ btc-trakkr/src/ws-get-blocks-mock-data.js
@@ -0,0 +1,63 @@
+export const blocksMockData = {
+  block: {
+    id: "000000000000000000027fcd5d68316f094a0936a19c13d7a52e3e027071b6e9",
+    height: 820735,
+    version: 536879104,
+    timestamp: 1702316066,
+    bits: 386150037,
+    nonce: 3319155074,
+    difficulty: 67305906902031.39,
+    merkle_root:
+      "9ce23b3164b735534d89d8bb1f1be1a025d598ed75540a841cd93fd53bd2aaeb",
+    tx_count: 3894,
+    size: 1650383,
+    weight: 3992801,
+    previousblockhash:
+      "000000000000000000016c29e80108c21927e0a755e8dc482b7eeb807451d84d",
+    mediantime: 1702313274,
+    stale: false,
+    extras: {
+      reward: 772572054,
+      coinbaseRaw:
+        "03ff850c04224877652f466f756e6472792055534120506f6f6c202364726f70676f6c642f1f3156560057170000000000",
+      orphans: [],
+      medianFee: 115.3805013037481,
+      feeRange: [
+        90.82062780269058, 94.68477987421383, 98.16404886561955,
+        106.72108843537416, 121.46507666098807, 150.354609929078,
+        466.313679245283,
+      ],
+      totalFees: 147572054,
+      avgFee: 37907,
+      avgFeeRate: 147,
+      utxoSetChange: 2306,
+      avgTxSize: 423.75,
+      totalInputs: 7396,
+      totalOutputs: 9702,
+      totalOutputAmt: 632046794954,
+      segwitTotalTxs: 3767,
+      segwitTotalSize: 1595763,
+      segwitTotalWeight: 3774429,
+      feePercentiles: null,
+      virtualSize: 998200.25,
+      coinbaseAddress: "bc1qxhmdufsvnuaaaer4ynz88fspdsxq2h9e9cetdj",
+      coinbaseSignature:
+        "OP_0 OP_PUSHBYTES_20 35f6de260c9f3bdee47524c473a6016c0c055cb9",
+      coinbaseSignatureAscii:
+        '\u0003ÿ\f\u0004"Hwe/Foundry USA Pool #dropgold/\u001f1VV\u0000W\u0017\u0000\u0000\u0000\u0000\u0000',
+      header:
+        "002000204dd8517480eb7e2b48dce855a7e02719c20801e8296c01000000000000000000ebaad23bd53fd91c840a5475ed98d525a0e11b1fbbd8894d5335b764313be29c22487765952e04178249d6c5",
+      utxoSetSize: null,
+      totalInputAmt: null,
+      pool: {
+        id: 111,
+        name: "Foundry USA",
+        slug: "foundryusa",
+      },
+      matchRate: 99.97,
+      expectedFees: 148188169,
+      expectedWeight: 3991812,
+      similarity: 0.983703688861282,
+    },
+  },
+};
