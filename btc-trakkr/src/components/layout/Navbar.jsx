d282c8f97a6b3cd1e1aa42d6b048b73dead19b00
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-17 03:44:06 -0500

    adding metadata for css selections

--- btc-trakkr/src/components/layout/Navbar.jsx
+++ btc-trakkr/src/components/layout/Navbar.jsx
@@ -13,29 +13,32 @@ const TrakkrNavbar = () => {
     <Navbar expand="lg" className="shadow-lg bg-body-tertiary">
         <Container fluid className="mx-4 d-flex justify-content-between">
         <Nav className="d-flex flex-row justify-content-evenly">
-            <Nav.Item className="mx-2">
-                <Link className="btn btn-warning" to="/">
-                    <BsHouseFill /> Home
+            <Nav.Item>
+                <Link to="/" className="btn rounded mx-2 text-decoration-none trakkr-nav-link">
+                    <BsHouseFill />
+                    <span className="ms-1">Home</span>
                 </Link>
                 </Nav.Item>
             </Nav>
 
-            <Navbar.Brand className="fs-2 fw-bold text-center text-reset text-decoration-none">
+            <Navbar.Brand className="d-flex align-items-center fs-2 mx-auto fw-bold text-center text-reset text-decoration-none">
                 <IconContext.Provider value={{ color: "orange", size: 40 }}>
                     <SiBitcoin id="btc-nav-icon" className="d-inline pe-2 bs-orange-500"/>
                 </IconContext.Provider>
-                TrakkR
+                <span className="fs-1">TrakkR</span>
             </Navbar.Brand>
 
             <Nav className="d-flex flex-row justify-content-evenly">
-                <Nav.Item className="mx-2">
-                    <Link className="btn btn-info" to="/mempool">
-                        <FaDatabase /> Mempool
+                <Nav.Item id="mempool-nav-link">
+                    <Link to="/mempool" className="btn rounded mx-2 text-decoration-none trakkr-nav-link">
+                        <FaDatabase />
+                        <span className="ms-1">Mempool</span>
                     </Link>
                 </Nav.Item>
-                <Nav.Item className="mx-2 rounded lightning-network-nav-link">
-                    <Link className="btn" to="/mempool">
-                        <AiFillThunderbolt /> Lightning
+                <Nav.Item id="lightning-network-nav-link">
+                    <Link to="/lightning" className="btn rounded mx-2 text-decoration-none trakkr-nav-link">
+                        <AiFillThunderbolt />
+                        <span className="ms-1">Lightning</span>
                     </Link>
                 </Nav.Item>
             </Nav>
