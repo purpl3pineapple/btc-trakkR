804779483368305f34428ad74ba911c0449d9a83
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-17 03:46:33 -0500

    mempool page layout so far

--- btc-trakkr/src/pages/MempoolPage.jsx
+++ btc-trakkr/src/pages/MempoolPage.jsx
@@ -8,6 +8,7 @@ import Badge from "react-bootstrap/Badge";
 import { useSelector, useDispatch } from "react-redux";
 import { useGetBlocksQuery, useGetRecentTxsQuery, useGetRecommendedFeesQuery } from "../api-services/mempool.service";
 import Row from "react-bootstrap/Row";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
 
 const MempoolPage = () => {
 
@@ -47,105 +48,130 @@ const { sendJsonMessage } = useWebSocket(REACT_APP_BLOCKCHAIN_WS_URL, {
 
   const mempoolTxns = useGetRecentTxsQuery();
 
-  console.log(mempoolTxns.data);
-
   return (
-    <Container fluid id="mempool" className="d-flex flex-column align-items-center w-100 vh-75 p-5">
-      <MempoolDataSection title="Mempool Recommended Fees" content={
+    <Container fluid id="mempool" className="overflow-auto position-static d-flex flex-column align-items-center w-100 vh-100 mh-100 p-5">
+      <Container fluid id="mempool-breadcrumbs" className="d-flex flex-row justify-content-around align-items-center p-4">
+        <Breadcrumb id="mempool-pg-nav-wrap" className="py-1 bg-dark rounded fs-4 w-50 d-flex justify-content-center align-items-center">
+          {/* <Breadcrumb.Item href="#mempool-recd-fees" className="mempool-bcrmb text-info">
+            <span>Fees</span>
+          </Breadcrumb.Item> */}
+          {/* <Breadcrumb.Item href="#mempool-recent-txs" className="mempool-bcrmb p-1">
+            <span className="mempool-pg-nav-link">Recent Txs</span>
+          </Breadcrumb.Item> */}
+          <Breadcrumb.Item href="#mempool-blocks" className="mempool-bcrmb p-1">
+            <span className="mempool-pg-nav-link">Blocks</span>
+          </Breadcrumb.Item>
+        </Breadcrumb>
+      </Container>
+      <MempoolDataRow id="mempool-recd-fees" data={[
+          {
+            title: 'Fastest Fee', 
+            value: mempoolFees.data?.fastestFee
+          },
+          {
+            title: 'Half-Hour Fee', 
+            value: mempoolFees.data?.halfHourFee
+          },
+          {
+            title: 'Hour Fee', 
+            value: mempoolFees.data?.hourFee
+          },
+          {
+            title: 'Economy Fee', 
+            value: mempoolFees.data?.economyFee
+          },
+          {
+            title: 'Minimum Fee', 
+            value: mempoolFees.data?.minimumFee
+          },
+        ]}/>
+      {/* <MempoolDataSection title="Mempool Recommended Fees" content={
         <Container className="h-100 w-75 p-4 d-flex flex-column align-items-center">
           <ListGroup className="mx-3 pe-0 align-items-center container">
             <ListGroup.Item className="w-100 d-flex justify-content-between align-items-start" variant="info">
               <div className="ms-2 me-auto">
-                Fastest Fee
+                
               </div>
               <Badge bg="dark" pill className="w-auto">
-              {mempoolFees.data?.fastestFee}
+              
               </Badge>
             </ListGroup.Item>
             <ListGroup.Item className="w-100 d-flex justify-content-between align-items-start" variant="info">
               <div className="ms-2 me-auto">
-                Half-Hour Fee
+                
               </div>
               <Badge bg="dark" pill className="w-auto">
-                {mempoolFees.data?.halfHourFee}
+                {}
               </Badge>
             </ListGroup.Item>
             <ListGroup.Item className="w-100 d-flex justify-content-between align-items-start" variant="info">
               <div className="ms-2 me-auto">
-                Hour Fee
+                
               </div>
               <Badge bg="dark" pill className="w-auto">
-                {mempoolFees.data?.hourFee}
+                {}
               </Badge>
             </ListGroup.Item>
             <ListGroup.Item className="w-100 d-flex justify-content-between align-items-start" variant="info">
               <div className="ms-2 me-auto">
-                Economy Fee
+                
               </div>
               <Badge bg="dark" pill className="w-auto">
-                {mempoolFees.data?.economyFee}
+                {}
               </Badge>
             </ListGroup.Item>
             <ListGroup.Item className="w-100 d-flex justify-content-between align-items-start" variant="info">
               <div className="ms-2 me-auto">
-                Minimum Fee
+                 Fee
               </div>
               <Badge bg="dark" pill className="w-auto">
-                {mempoolFees.data?.minimumFee}
+                {}
               </Badge>
             </ListGroup.Item>
           </ListGroup>
         </Container>
-      }/>
-      <MempoolDataSection title="Mempool Recent Transactions" content={
+      }/> */}
+      <MempoolDataSection id="mempool-recent-txs" title="Mempool Recent Transactions" content={
         <Container className="h-100 w-100 p-4 d-flex flex-column align-items-center">
-          <ListGroup className="mx-3 pe-0 align-items-center container">
-            {mempoolTxns.data.map(txn => <ListGroup.Item className="w-100 d-flex justify-content-between align-items-start" variant="info">
+          <ListGroup as="ol" className="mx-3 pe-0 align-items-center container h-100">
+            {mempoolTxns.status === 'fulfilled' && mempoolTxns.data?.map((txn, idx) => <ListGroup.Item as="li" key={idx} action className="text-break font-monospace w-100 d-flex justify-content-between align-items-start" variant="info">
               <div className="ms-2 me-auto p-3 d-flex flex-column align-items-start">
                 <div className="fw-bold fs-7 p-2">{txn.txid}</div>
               </div>
-              <Row className="container h-100 justify-content-evenly align-items-center">
-              <Badge bg="dark" pill className="w-auto">
-                vSize: {txn.vsize} bytes
+              <Row className="container m-auto justify-content-around align-items-center">
+              <Badge bg="dark" pill className="w-auto d-flex justify-content-evenly align-items-center">
+                <span className="mempool-txn-data-hdr me-2 fw-bold">vSize:</span> <span>{txn.vsize} bytes</span>
               </Badge>
-              <Badge bg="dark" pill className="w-auto">
-                Fee: ${txn.fee}
+              <Badge bg="dark" pill className="w-auto d-flex justify-content-evenly align-items-center">
+                <span className="mempool-txn-data-hdr me-2 fw-bold">Fee:</span> ${txn.fee}
               </Badge>
-              <Badge bg="dark" pill className="w-auto">
-                Amount: ${txn.value}
+              <Badge bg="dark" pill className="w-auto d-flex justify-content-evenly align-items-center">
+                <span className="mempool-txn-data-hdr me-2 fw-bold">Amount:</span> ${txn.value}
               </Badge>
               </Row>
             </ListGroup.Item>
             )}
-            {/* <ListGroup.Item className="w-100 d-flex justify-content-between align-items-start" variant="info">
-              <div className="ms-2 me-auto">
-                Minimum Fee
-              </div>
-              <Badge bg="dark" pill className="w-auto">
-                {mempoolFees.data.minimumFee}
-              </Badge>
-            </ListGroup.Item> */}
           </ListGroup>
         </Container>
       }/>
-      <MempoolDataSection title="Mempool Block Info" content={
+      <MempoolDataSection id="mempool-blocks" title="Mempool Block Info" content={
         <Table responsive striped bordered hover variant="dark-info">
           <thead>
             <tr>
-              <th>#</th>
-              <th>ID</th>
-              <th>Pool Name</th>
-              <th>Median Fee</th>
-              <th>Match Rate</th>
-              <th>Size</th>
-              <th>Txn Count</th>
-              <th>Timestamp</th>
-              <th>Median Time</th>
-              <th>Nonce</th>
+              <th><span className="mempool-data-table-hdr"></span></th>
+              <th><span className="mempool-data-table-hdr">ID</span></th>
+              <th><span className="mempool-data-table-hdr">Pool Name</span></th>
+              <th><span className="mempool-data-table-hdr">Median Fee</span></th>
+              <th><span className="mempool-data-table-hdr">Match Rate</span></th>
+              <th><span className="mempool-data-table-hdr">Size</span></th>
+              <th><span className="mempool-data-table-hdr">Txn Count</span></th>
+              <th><span className="mempool-data-table-hdr">Timestamp</span></th>
+              <th><span className="mempool-data-table-hdr">Median Time</span></th>
+              <th><span className="mempool-data-table-hdr">Nonce</span></th>
             </tr>
           </thead>
           <tbody className="fs-7">
-            {mempoolBlocks.status === 'fulfilled' && mempoolBlocks.data.map((block, idx) => <tr key={idx}>
+            {mempoolBlocks.status === 'fulfilled' && mempoolBlocks.data?.map((block, idx) => <tr key={idx}>
               <td>{idx + 1}</td>
               <td>{block.id}</td>
               <td>{block.extras.pool.name}</td>
@@ -160,9 +186,9 @@ const { sendJsonMessage } = useWebSocket(REACT_APP_BLOCKCHAIN_WS_URL, {
           </tbody>
         </Table>
       }/>
-      <MempoolDataSection title="Mempool Stats" content={
+      {/* <MempoolDataSection title="Mempool Stats" content={
         <>
-        <Container className="w-50 ps-2 pb-5 d-flex flex-column align-items-center">
+          <Container className="w-50 ps-2 pb-5 d-flex flex-column align-items-center">
             <h1 className="fs-4 fw-bold text-center">Hash</h1>
             <ListGroup className="mx-3 pe-0 align-items-center container">
               <ListGroup.Item className="w-100" variant="info">
@@ -242,7 +268,7 @@ const { sendJsonMessage } = useWebSocket(REACT_APP_BLOCKCHAIN_WS_URL, {
               </ListGroup.Item>
             </ListGroup>
           </Container>
-          {/* <Container className="h-100 w-50 p-4 d-flex flex-column align-items-center">
+          <Container className="h-100 w-50 p-4 d-flex flex-column align-items-center">
             <h1 className="fs-4 fw-bold text-center">Fees</h1>
             <ListGroup className="mx-3 pe-0 align-items-center container">
               <ListGroup.Item className="w-100 d-flex justify-content-between align-items-start" variant="info">
@@ -267,19 +293,11 @@ const { sendJsonMessage } = useWebSocket(REACT_APP_BLOCKCHAIN_WS_URL, {
                 </Badge>
               </ListGroup.Item>
             </ListGroup>
-          </Container> */}
+          </Container>
           </Container>
         </>
-      }/>
-      <MempoolDataSection title="Mempool Blocks" content={
-        <MempoolDataRow data={[
-          {title: 'Block ID 1', value: '11111111111111111111111111111111'},
-          {title: 'Block ID 2', value: '11111111111111111111111111111111'},
-          {title: 'Block ID 3', value: '11111111111111111111111111111111'},
-          {title: 'Block ID 4', value: '11111111111111111111111111111111'},
-        ]}/>
-      }/>
-      <MempoolDataSection title="List Group Sample" content={
+      }/> */}
+      {/* <MempoolDataSection title="List Group Sample" content={
         <ListGroup className="mx-5 align-items-center">
           <ListGroup.Item className="w-75 d-flex justify-content-between align-items-start" variant="info">
           <div className="ms-2 me-auto">
@@ -290,7 +308,7 @@ const { sendJsonMessage } = useWebSocket(REACT_APP_BLOCKCHAIN_WS_URL, {
           </Badge>
           </ListGroup.Item>
         </ListGroup>
-      }/>
+      }/> */}
     </Container>
   );
 };
