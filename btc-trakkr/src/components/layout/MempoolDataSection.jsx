033dfc0d144a4d820ac3a453573e5f98b8694929
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-17 03:45:35 -0500

    new format for data section

--- btc-trakkr/src/components/layout/MempoolDataSection.jsx
+++ btc-trakkr/src/components/layout/MempoolDataSection.jsx
@@ -1,10 +1,10 @@
 import Container from 'react-bootstrap/Container';
 
-const MempoolDataSection = ({ title, content }) => {
+const MempoolDataSection = ({ id = '', title, content }) => {
   return (
-    <Container className="mempool-data-container d-flex flex-column justify-content-center text-center p-3 my-5 bg-body-tertiary rounded">
-      <h1 className="mb-3 container-fluid mempool-data-header">
-        <span className="bg-info fs-2 fw-bold p-3 my-4 rounded d-inline-block text-decoration-underline shadow text-reset">
+    <Container id={id} className="mempool-data-container d-flex flex-column justify-content-center text-center p-3 my-5 bg-body-tertiary rounded">
+      <h1 className="mb-3 container-fluid text-dark mempool-data-header-ctnr">
+        <span className="mempool-data-header fs-2 fw-bold p-3 my-4 rounded d-inline-block shadow text-reset">
           {title}
         </span>
       </h1>
