d7d0104f51e5564d57334efb2e7e05544c47795a
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-17 03:44:37 -0500

    new format for data values on page

--- btc-trakkr/src/components/MempoolDataValue.jsx
+++ btc-trakkr/src/components/MempoolDataValue.jsx
@@ -1,17 +1,28 @@
+import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
 
-const MempoolDataValue = ({ title, value }) => {
-  return (
-    <Card className='mempool-card h-auto m-3 p-2 flex-shrink-1 rounded-circle'>
+{/* <Card className='mempool-card h-auto m-3 p-2 flex-shrink-1 rounded'>
       <Card.Body>
-        <Card.Title className="fw-bold fs-6">
+        <Card.Title className="fw-bold fs-6 text-nowrap text-center text-decoration-underline lh-md">
           {title}
         </Card.Title>
-        <Card.Text className="fs-7">
+        <Card.Text className="fs-5">
           {value}
         </Card.Text>
       </Card.Body>
-    </Card>
+    </Card> */}
+
+const MempoolDataValue = ({ title, value }) => {
+  return (
+    <ListGroup.Item className='d-flex flex-column justify-content-center'>
+      <div className="mempool-fee-header ms-2 me-auto fs-6 fw-bold">
+        {title}
+      </div>
+      <Badge bg="dark" pill>
+        ${value}
+      </Badge>
+    </ListGroup.Item>
   );
 };
 
