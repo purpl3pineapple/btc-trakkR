9227a67c6b08ccc02659686f10cb42db1a89ab8a
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-21 23:06:28 -0500

    re-organizing navbar

--- btc-trakkr/src/components/layout/Navbar.jsx
+++ btc-trakkr/src/components/layout/Navbar.jsx
@@ -9,39 +9,30 @@ import { AiFillThunderbolt } from "react-icons/ai";
 const TrakkrNavbar = () => {
 
     return (
-        <Navbar expand="lg" className="shadow-lg bg-body-tertiary">
-            <Container fluid className="mx-4 d-flex justify-content-between">
-            <Nav className="d-flex flex-row justify-content-evenly">
-                <Nav.Item>
-                    <Link to="/" className="btn rounded mx-2 text-decoration-none trakkr-nav-link">
-                        <BsHouseFill />
-                        <span className="ms-1">Home</span>
+        <Navbar expand="md" sticky="top" className="shadow-lg bg-body-tertiary">
+            <span className="ps-3 me-4">
+                <Navbar.Brand className="d-flex align-items-center fs-2 me-auto fw-bold text-center text-reset text-decoration-none">
+                    <Link to="/" className="text-decoration-none text-reset" role="button" tabIndex="0">
+                        <IconContext.Provider value={{ color: "orange", size: 40 }}>
+                            <SiBitcoin id="btc-nav-icon" className="d-inline pe-2 bs-orange-500"/>
+                        </IconContext.Provider>
+                        <span className="fs-1">TrakkR</span>
                     </Link>
-                    </Nav.Item>
-                </Nav>
-
-                <Navbar.Brand className="d-flex align-items-center fs-2 mx-auto fw-bold text-center text-reset text-decoration-none">
-                    <IconContext.Provider value={{ color: "orange", size: 40 }}>
-                        <SiBitcoin id="btc-nav-icon" className="d-inline pe-2 bs-orange-500"/>
-                    </IconContext.Provider>
-                    <span className="fs-1">TrakkR</span>
                 </Navbar.Brand>
-
-                <Nav className="d-flex flex-row justify-content-evenly">
-                    <Nav.Item id="mempool-nav-link">
-                        <Link to="/mempool" className="btn rounded mx-2 text-decoration-none trakkr-nav-link">
-                            <FaDatabase />
-                            <span className="ms-1">Mempool</span>
-                        </Link>
-                    </Nav.Item>
-                    <Nav.Item id="lightning-network-nav-link">
-                        <Link to="/lightning" className="btn rounded mx-2 text-decoration-none trakkr-nav-link">
-                            <AiFillThunderbolt />
-                            <span className="ms-1">Lightning</span>
-                        </Link>
-                    </Nav.Item>
+            </span>
+            <Navbar.Toggle aria-controls="trakkr-nav" />
+            <Navbar.Collapse id="trakkr-nav">
+                <Nav className="me-auto">
+                    <Link id="mempool-nav-link" to="/mempool" role="button" tabIndex="0" className="nav-link mx-2 fw-bold trakkr-nav-link">
+                        <FaDatabase />
+                        <span className="ms-1">Mempool</span>
+                    </Link>
+                    <Link id="lightning-network-nav-link" to="/lightning" role="button" tabIndex="0" className="nav-link mx-2 fw-bold trakkr-nav-link">
+                        <AiFillThunderbolt />
+                        <span className="ms-1">Lightning</span>
+                    </Link>
                 </Nav>
-            </Container>
+            </Navbar.Collapse>
         </Navbar>
     );
 };
