cba787e4fcccd39034695ae4468f0b0d27ce23cf
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-21 23:07:41 -0500

    mock data for Blockchain.com WS block msg

---
+++ btc-trakkr/src/api-services/mock-data/blockchain.ws.block.js
@@ -0,0 +1,31 @@
+const blockchainWebsocketGetBlockMock = {
+  op: "block",
+  x: {
+    txIndexes: [],
+    nTx: 4266,
+    estimatedBTCSent: 0,
+    totalBTCSent: 0,
+    reward: 1001240966,
+    size: 1645386,
+    weight: 3993336,
+    blockIndex: 822118,
+    prevBlockIndex: 822117,
+    height: 822118,
+    hash: "000000000000000000016b5d3bb004fde6e9c559a370b667541370808e62c812",
+    mrklRoot:
+      "b16a64eb31dcb2d562f52662a47ddae122d81080c5dac58c4f9fc7cac2b54157",
+    difficulty: 0,
+    version: 551550976,
+    time: 1703095782,
+    bits: 386150037,
+    nonce: 4236799128,
+    foundBy: {
+      description: "",
+      ip: "",
+      link: "",
+      time: 0,
+    },
+  },
+};
+
+//000000000000000000027289cf677ce1fcce244b184904b25965221846270024
