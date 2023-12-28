32a6f37e8125aca514775fca540956bc55f1d3ff
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-28 04:54:53 -0500

    removing unnecessary slices

--- btc-trakkr/src/app/store.js
+++ btc-trakkr/src/app/store.js
@@ -1,22 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import sliceBTC from "../context/BTC-price/btcPrice.slice";
-import mempoolAPI from "../api-services/mempool.service";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import sliceMempoolBlocks from "../context/mempool/mempool.blocks.slice";
-import sliceMempoolRecommendedFees from "../context/mempool/mempool.fees.slice";
-import sliceMempoolTxns from "../context/mempool/mempool.txns.slice";
+import sliceBTC from "../context/BTC-price/btcPrice.slice";
+import btcMiddleware from "../context/BTC-price/btcPriceMiddleware";
 import sliceLightning from "../context/lightning/lightning.slice";
 import sliceMempool from "../context/mempool/mempool.slice";
-import btcMiddleware from "../context/BTC-price/btcPriceMiddleware";
+import mempoolAPI from "../api-services/mempool.service";
 
 export const store = configureStore({
     reducer: {
         BTC: sliceBTC.reducer,
         mempool: sliceMempool.reducer,
         mempoolAPI: mempoolAPI.reducer,
-        mempoolBlocks: sliceMempoolBlocks.reducer,
-        mempoolFees: sliceMempoolRecommendedFees.reducer,
-        mempoolTxns: sliceMempoolTxns.reducer,
         lightning: sliceLightning.reducer,
     },
     immutableCheck: false,
