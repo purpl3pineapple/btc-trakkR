e199f10e0d3bfcdbed7cf835d45c0d8a506725ba
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-28 04:58:50 -0500

    mempool slice up-to-date

--- btc-trakkr/src/context/mempool/mempool.slice.js
+++ btc-trakkr/src/context/mempool/mempool.slice.js
@@ -1,115 +1,141 @@
 import TrakkrDataSlice from "../../components/TrakkrDataSlice";
 
-const sliceMempool = new TrakkrDataSlice('mempool', {
+const sliceMempool = new TrakkrDataSlice(
+  "mempool",
+  {
     liveStats: {},
     info: {
-        loaded: null,
-        size: null,
-        bytes: null,
-        usage: null,
-        maxmempool: null,
-        mempoolminfee: null,
-        minrelaytxfee: null,
-        incrementalrelayfee: null,
-        unbroadcastcount: null,
-        fullrbf: null,
-        vBytesPerSecond: null,
-        loading: true
+      loaded: null,
+      size: null,
+      bytes: null,
+      usage: null,
+      maxmempool: null,
+      mempoolminfee: null,
+      minrelaytxfee: null,
+      incrementalrelayfee: null,
+      unbroadcastcount: null,
+      fullrbf: null,
+      vBytesPerSecond: null,
+      loading: true,
     },
     da: {
-        progressPercent: null,
-        difficultyChange: null,
-        estimatedRetargetDate: null,
-        remainingBlocks: null,
-        remainingTime: null,
-        previousRetarget: null,
-        previousTime: null,
-        nextRetargetHeight: null,
-        timeAvg: null,
-        timeOffset: null,
-        expectedBlocks: null,
-        loading: true,
+      progressPercent: null,
+      difficultyChange: null,
+      estimatedRetargetDate: null,
+      remainingBlocks: null,
+      remainingTime: null,
+      previousRetarget: null,
+      previousTime: null,
+      nextRetargetHeight: null,
+      timeAvg: null,
+      timeOffset: null,
+      expectedBlocks: null,
+      loading: true,
     },
     blocks: {
-        newest: null,
-        mempool: null,
-        loading: true
+      newBlockDetected: false,
+      newest: null,
+      upcoming: null,
+      current: null,
+      modals: [],
+      loading: true,
     },
     txs: [],
     fees: {
-        totalFee: null,
-        fastestFee: null,
-        halfHourFee: null,
-        hourFee: null,
-        economyFee: null,
-        minimumFee: null,
-        loading: true
+      totalFee: null,
+      fastestFee: null,
+      halfHourFee: null,
+      hourFee: null,
+      economyFee: null,
+      minimumFee: null,
+      loading: true,
     },
     conversions: null,
-    loading: true
-},
-{   
+    loading: true,
+  },
+  {
     updateLiveStats: (state, action) => {
-
-        state.liveStats = action.payload.data;
-        state.info.loading = false;
-        state.loading = false;
+      state.liveStats = action.payload.data;
+      state.info.loading = false;
+      state.loading = false;
     },
 
     updateInfo: (state, action) => {
-
-        state.info = {
-            ...action.payload.mempoolInfo,
-            vBytesPerSecond: action.payload.vBytesPerSecond
-        };
-        state.info.loading = false;
-        state.loading = false;
+      state.info = {
+        ...action.payload.mempoolInfo,
+        vBytesPerSecond: action.payload.vBytesPerSecond,
+      };
+      state.info.loading = false;
+      state.loading = false;
     },
 
     updateDiffAdj: (state, action) => {
-
-        state.da = action.payload.da;
-        state.da.loading = false;
-        state.loading = false;
+      state.da = action.payload.da;
+      state.da.loading = false;
+      state.loading = false;
     },
 
     updateBlocks: (state, action) => {
-
-        state.blocks.mempool = action.payload.blocks;
-        state.blocks.loading = false;
-        state.loading = false;
+      state.blocks.current = action.payload.blocks;
+      state.blocks.modals = action.payload.blocks.map((block) => {
+        return { id: block.id, show: false };
+      });
+      state.blocks.loading = false;
+      state.loading = false;
     },
 
     updateNewestBlock: (state, action) => {
+      state.blocks.newest = action.payload.block;
+      state.blocks.modals = [
+        { id: action.payload.block.id, show: false },
+        ...state.blocks.modals,
+      ];
+      state.blocks.loading = false;
+      state.loading = false;
+    },
 
-        state.blocks.newest = action.payload.block;
-        state.blocks.loading = false;
-        state.loading = false;
+    updateNewBlockDetectedStatus: (state, action) => {
+      state.blocks.newBlockDetected = action.payload;
+      state.blocks.loading = false;
+      state.loading = false;
+    },
+
+    updateUpcomingBlocks: (state, action) => {
+      state.blocks.upcoming = action.payload;
+      state.blocks.loading = false;
+      state.loading = false;
     },
 
     updateTxs: (state, action) => {
-
-        state.txs = action.payload.transactions;
-        //state.txs.loading = false;
-        state.loading = false;
+      state.txs = action.payload.transactions;
+      state.loading = false;
     },
 
     updateFees: (state, action) => {
-
-        state.fees = {
-            ...action.payload.fees, 
-            totalFee: action.payload.totalFee,
-            loading: false
-        };
-        state.loading = false;
+      state.fees = {
+        ...action.payload.fees,
+        totalFee: action.payload.totalFee,
+        loading: false,
+      };
+      state.loading = false;
     },
 
     updateConversions: (state, action) => {
-
-        state.conversions = action.payload.conversions;
-        state.loading = false;
+      state.conversions = action.payload.conversions;
+      state.loading = false;
     },
-});
 
+    controlModal: (state, action) => {
+      state.blocks.modals = state.blocks.modals.map((modal) =>
+        modal.id === action.payload.id
+          ? { id: modal.id, show: action.payload.show }
+          : modal
+      );
+      
+      state.blocks.loading = false;
+      state.loading = false;
+    },
+  }
+);
 
 export default sliceMempool;
