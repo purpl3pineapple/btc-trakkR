84833d708782b8db193ca0dc46a3c9852ebcb24d
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-30 10:53:20 -0500

    adding coincap api service & mempool middleware to store

--- btc-trakkr/src/app/store.js
+++ btc-trakkr/src/app/store.js
@@ -5,19 +5,24 @@ import btcMiddleware from "../context/BTC-price/btcPriceMiddleware";
 import sliceLightning from "../context/lightning/lightning.slice";
 import sliceMempool from "../context/mempool/mempool.slice";
 import mempoolAPI from "../api-services/mempool.service";
+import mempoolMiddleware from "../context/mempool/mempoolMiddleware";
+import coincapAPI from "../api-services/coincap.service";
 
 export const store = configureStore({
     reducer: {
         BTC: sliceBTC.reducer,
+        coincapAPI: coincapAPI.reducer,
+        lightning: sliceLightning.reducer,
         mempool: sliceMempool.reducer,
         mempoolAPI: mempoolAPI.reducer,
-        lightning: sliceLightning.reducer,
     },
     immutableCheck: false,
     serializableCheck: false,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware()
     .concat(mempoolAPI.middleware)
     .concat(btcMiddleware.middleware)
+    .concat(mempoolMiddleware.middleware)
+    .concat(coincapAPI.middleware)
 });
 
 setupListeners(store.dispatch);
