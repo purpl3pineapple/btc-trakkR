d76eab58c1926b7116e995420d58c3853643e948
purpl3pineapple <opaloctopus1005@gmail.com>
2023-12-30 10:58:37 -0500

    new layout experimentation (adding stats to display)

--- btc-trakkr/src/pages/HomePage.jsx
+++ btc-trakkr/src/pages/HomePage.jsx
@@ -3,45 +3,83 @@ import BitcoinPrice from "../components/BitcoinPrice";
 import useWebSocket from "react-use-websocket";
 import { useDispatch, useSelector } from "react-redux";
 import sliceBTC from "../context/BTC-price/btcPrice.slice";
+import { useEffect } from "react";
+import coincapAPI from "../api-services/coincap.service";
+import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Badge from "react-bootstrap/Badge";
 
 const HomePage = () => {
+  const { updatePrice } = sliceBTC.actions;
 
-    const { updatePrice } = sliceBTC.actions;
+  const btcCurrent = useSelector((state) => state.BTC);
 
-    const currentPrice = useSelector(state => state.BTC);
+  const { stats } = btcCurrent;
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
+  useWebSocket(process.env.REACT_APP_COINCAP_WS_URL, {
+    onMessage: (msg) => {
+      const { bitcoin } = JSON.parse(msg.data);
 
-    useWebSocket(process.env.REACT_APP_COINCAP_WS_URL, {
+      dispatch(updatePrice({ BTC: bitcoin, ...btcCurrent }));
+    },
 
-        onMessage: msg => {
+    shouldReconnect: () => true,
+  });
 
-            const { bitcoin } = JSON.parse(msg.data);
+  useEffect(() => {
+    const btcStats = dispatch(coincapAPI.endpoints.getBitcoinStats.initiate());
 
-            dispatch(updatePrice({ BTC: bitcoin, ...currentPrice }));
-        },
+    return btcStats.unsubscribe;
+  }, [dispatch]);
 
-        shouldReconnect: () => true,
-    });
-    
-
-    return (
-        <main id="main" className="container-fluid mt-5 d-flex justify-content-center flex-grow-1 pb-5">
-            <Card id="btc-price-display" className="text-center w-50 bg-dark" style={{ 
-                    maxHeight: "250px" 
-                }}>
-                <Card.Header id="btc-price-header" className="fs-4 fw-bold">
-                    Current BTC Price:
-                </Card.Header>
-                <Card.Body id="btc-price-body" className="bg-dark">
-                    <Card.Text id="btc-price-wrap" className="mh-100 py-5">
-                        <BitcoinPrice />
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </main>
-    );
+  return (
+    <Row className="w-100 p-3">
+        <Col className="p-1 mx-4">
+          <Card className="shadow-lg w-75 mx-auto">
+            <Card.Header className="fw-bolder extras-list-hdr text-center fs-1">
+              {stats === null ? '' : stats.symbol}
+            </Card.Header>
+            <ListGroup variant="flush">
+              <ListGroup.Item
+                variant="primary"
+                className="py-4 d-flex flex-sm-column flex-md-row justify-content-sm-center justify-content-md-between align-items-md-center"
+              >
+                <Badge bg="dark" pill className="w-50 px-3 mx-auto fs-2">
+                  <BitcoinPrice />
+                </Badge>
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                variant="dark"
+                className="fs-5 d-flex flex-sm-column flex-md-row justify-content-sm-center justify-content-md-between align-items-md-center"
+              >
+                <h3 className="fw-bolder text-center text-info">Supply:</h3>
+                  <span className="text-break font-monospace">{stats === null ? '' : stats.supply}</span>
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                variant="dark"
+                className="fs-5 d-flex flex-sm-column flex-md-row justify-content-sm-center justify-content-md-between align-items-md-center"
+              >
+                <h3 className="fw-bolder text-center text-info">Market Cap:</h3>
+                  <span className="text-break font-monospace">{stats === null ? '' : stats.marketCapUsd}</span>
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                variant="dark"
+                className="fs-5 d-flex flex-sm-column flex-md-row justify-content-sm-center justify-content-md-between align-items-md-center"
+              >
+                <h3 className="fw-bolder text-center text-info">Market Volume:</h3>
+                  <span className="text-break font-monospace">{stats === null ? '' : stats.volumeUsd24Hr}</span>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+  );
 };
 
 export default HomePage;
